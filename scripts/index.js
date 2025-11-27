1f04277f591a021e9123b566b0d7412e6b719bc8
kakadacancer-code <kakadacancer@gmail.com>
2025-11-27 16:13:22 +0700

    done

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-console.log("Main JavaScript");
-
-fetch("../public/data/properties.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.error("Error fetching JSON:", error));
